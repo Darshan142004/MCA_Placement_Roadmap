2f80d7c87788108a322ec85c8193cb0354fa7dee
Darshan Bhagwat <darshanbhagwat14@gmail.com>
2025-11-10 23:41:38 +0530

    array basic

--- MCA_DSA/Array/ArrayOperation.py
+++ MCA_DSA/Array/ArrayOperation.py
@@ -12,4 +12,23 @@ arr2=arr1; # copy
 for i in arr2:
     print(i);
 
+#reverse
+start=0;
+end=len(arr1)-1;
+while(start<end):
+    arr1[start],arr1[end]=arr1[end],arr1[start];
+    start+=1;
+    end-=1;
+    
+for i in arr1:
+    print(i);
+
+arr3=(3,5,2,4,6,235,4);
+summ=0;
+for i in arr3:
+    summ=summ+i;
+    avgg=summ/(len(arr3));
+print("sum is=",summ," AVG=",avgg);
+print(max(arr3));
+print(min(arr3));
 
