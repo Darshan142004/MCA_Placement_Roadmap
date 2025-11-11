ffe55758cc267fd9cd84523c934a53e3b3c88dbb
Darshan Bhagwat <darshanbhagwat14@gmail.com>
2025-11-11 23:18:48 +0530

    binary

---
+++ MCA_DSA/Array/bin.py
@@ -0,0 +1,19 @@
+arr=(10,20,30,40,50,60,70,80,90,100,110,115,120,125,130);
+x=90;
+low=0;
+high=len(arr)-1;
+found=False;
+
+while(low<=high):
+    mid=(low+high)//2;
+    if arr[mid]==x:
+        print("Element found");
+        found=True;
+        break;
+    elif arr[mid]<x:
+        low+=1;
+    else:
+        high-=1;
+
+if not found:
+    print("Not presents")
