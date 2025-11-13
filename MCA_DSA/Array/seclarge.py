25df472a45ce8e3ecdc20b4a8a64accaa2872d35
Darshan Bhagwat <darshanbhagwat14@gmail.com>
2025-11-13 17:14:54 +0530

    secondLargest

---
+++ MCA_DSA/Array/seclarge.py
@@ -0,0 +1,15 @@
+arr=[20,23,25,98,57,43];
+
+if arr[0]>arr[1]:
+    first,sec=arr[0],arr[1];
+else:
+    first,sec=arr[1],arr[0];
+
+for i in range(len(arr)):
+    if arr[i]>first:
+        sec=first;
+        first=arr[i];
+    elif arr[i]>sec and arr[i]!=first:
+        sec=arr[i];
+print(first)
+print(sec);
