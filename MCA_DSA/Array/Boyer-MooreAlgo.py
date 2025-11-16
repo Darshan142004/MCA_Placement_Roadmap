baa210104fdd567eef4c921cfb8fe7330bc918c7
Darshan Bhagwat <darshanbhagwat14@gmail.com>
2025-11-16 23:06:45 +0530

    Boyer-Moore Algorithm

---
+++ MCA_DSA/Array/Boyer-MooreAlgo.py
@@ -0,0 +1,14 @@
+arr = [2, 2, 1, 1, 2]
+
+candidate = count = 0
+
+for n in arr:
+    if count == 0:
+        candidate = n
+    count += (1 if n == candidate else -1)
+
+# verify candidate
+if arr.count(candidate) > len(arr)//2:
+    print("Majority:", candidate)
+else:
+    print("No majority element")
