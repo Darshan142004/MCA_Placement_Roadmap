f40e2bacea5422bca8c4f73bcf1e5a4c9c7cac9e
Darshan Bhagwat <darshanbhagwat14@gmail.com>
2025-11-16 23:07:16 +0530

    PrefixSuffix

---
+++ MCA_DSA/Array/PrefixSuffix.py
@@ -0,0 +1,20 @@
+arr = [1, 2, 3, 4]
+n = len(arr)
+
+prefix = [1] * n
+suffix = [1] * n
+result = [1] * n
+
+# Build prefix
+for i in range(1, n):
+    prefix[i] = prefix[i-1] + arr[i-1]
+
+# Build suffix
+for i in range(n-2, -1, -1):
+    suffix[i] = suffix[i+1] * arr[i+1]
+
+# Build result
+for i in range(n):
+    result[i] = prefix[i] * suffix[i]
+
+print(result)
