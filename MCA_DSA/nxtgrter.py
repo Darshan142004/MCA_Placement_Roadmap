c92e7e2ac1130008e7a51e249cecc66d1b63ffce
Darshan Bhagwat <darshanbhagwat14@gmail.com>
2025-11-17 22:26:31 +0530

    nxt greater

---
+++ MCA_DSA/nxtgrter.py
@@ -0,0 +1,13 @@
+arr = [4, 5, 2, 25]
+n = len(arr)
+
+stack = []
+result = [-1] * n
+
+for i in range(n):
+    while stack and arr[i] > arr[stack[-1]]:
+        idx = stack.pop()
+        result[idx] = arr[i]
+    stack.append(i)
+
+print(result)
