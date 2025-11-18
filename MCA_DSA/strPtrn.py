05415d35364368ef28f2740322096f65db461528
Darshan Bhagwat <darshanbhagwat14@gmail.com>
2025-11-18 23:10:46 +0530

    chnged

--- MCA_DSA/strPtrn.py
+++
@@ -1,9 +0,0 @@
-n=int(input()) #5
-for i in range(n): #i=1 ,n=5
-    for m in range(n):# m=0 ,
-        if(i<n-(m+1)):#1<5-1
-           print(" ",end=" ")
-        else:
-           print("#",end=" ")
-    print("")
-   
