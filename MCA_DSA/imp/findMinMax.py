5281761d8e8ed19ebf9d031ae51f4f814e36eaf9
Darshan Bhagwat <darshanbhagwat14@gmail.com>
2025-11-21 20:05:33 +0530

    max and min without sort

---
+++ MCA_DSA/imp/findMinMax.py
@@ -0,0 +1,13 @@
+
+arr = [10, 20, 5, 8, 40, 3]
+
+largest = smallest = arr[0]
+
+for x in arr:
+    if x > largest:
+        largest = x
+    if x < smallest:
+        smallest = x
+
+print("Largest:", largest)
+print("Smallest:", smallest)
