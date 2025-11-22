d6072ebf603a73f37956b1a2aed851e97a8a937e
Darshan Bhagwat <darshanbhagwat14@gmail.com>
2025-11-22 20:44:44 +0530

    Longest word in sentence

---
+++ MCA_DSA/imp/longestword.py
@@ -0,0 +1,16 @@
+s = "I am learning data structures in python"
+
+max_len = 0
+current = ""
+longest = ""
+
+for ch in s + " ":      # add space to process last word
+    if ch != " ":
+        current += ch
+    else:
+        if len(current) > max_len:
+            max_len = len(current)
+            longest = current
+        current = ""
+
+print("Longest word:", longest)
