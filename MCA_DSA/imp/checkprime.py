e38f44f212cdb1df29e89f78cd2e342bc0650073
Darshan142004 <darshanbhagwat14@gmail.com>
2026-01-02 22:09:42 +0530

    Update

--- MCA_DSA/imp/checkprime.py
+++ MCA_DSA/imp/checkprime.py
@@ -6,6 +6,5 @@ else:
     for i in range(2, int(n**0.5)+1):
         if n % i == 0:
             is_prime = False
-            break
-        
+            break        
 print("Prime" if is_prime else "Not Prime")
