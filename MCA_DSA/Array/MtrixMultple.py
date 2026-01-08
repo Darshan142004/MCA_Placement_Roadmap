b6bcbecd32eb99b60034a8424443c080c1b153ed
Darshan142004 <darshanbhagwat14@gmail.com>
2026-01-08 15:26:58 +0530

    Matrix multi

--- MCA_DSA/Array/MtrixMultple.py
+++ MCA_DSA/Array/MtrixMultple.py
@@ -1,9 +1,18 @@
-rows=int(input("Enter number of rows"))
-matrix=[]
+# rows=int(input("Enter number of rows"))
+# matrix=[]
 
-print("Enter each row");
-for i in range(rows):
-    row=list(map(int,input().split()))
-    matrix.append(row)
+# print("Enter each row");
+# for i in range(rows):
+#     row=list(map(int,input().split()))
+#     matrix.append(row)
 
-print("matrix=",matrix);
+# print("matrix=",matrix);
+
+import numpy as np
+
+# Just use @ operator!
+A = [[1, 2], [3, 4]]
+B = [[5, 6], [7, 8]]
+
+result = np.array(A) @ np.array(B)
+print(result)
