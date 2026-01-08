0ce119249b48b86840f81e5818b6f260e4d6a89b
Darshan142004 <darshanbhagwat14@gmail.com>
2026-01-08 15:31:49 +0530

    updaed

--- MCA_DSA/Array/MtrixMultple.py
+++ MCA_DSA/Array/MtrixMultple.py
@@ -9,8 +9,6 @@
 # print("matrix=",matrix);
 
 import numpy as np
-
-
 A = [[1, 2], [3, 4]]
 B = [[5, 6], [7, 8]]
 
--- MCA_DSA/Array/revArr.py
+++ MCA_DSA/Array/revArr.py
@@ -1,3 +1,3 @@
-my_array = [1, 2, 3, 4, 5]
+my_array = [1, 2, 3, 4, 5,"Programmer","JAVA","hello"]
 reversed_arr = my_array[::-1]
 print(reversed_arr)
