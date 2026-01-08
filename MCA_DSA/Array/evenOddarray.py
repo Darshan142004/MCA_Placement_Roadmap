7586d2c82bf2950312f2fa2151dd26d6a26bd847
Darshan142004 <darshanbhagwat14@gmail.com>
2026-01-08 14:57:52 +0530

    Even and Odd array

---
+++ MCA_DSA/Array/evenOddarray.py
@@ -0,0 +1,11 @@
+arr=[2,3,4,5,6,7,8,9,11,23,43,45,67,87,88]
+arrE=[]
+arrO=[]
+for i in arr:
+    if(i % 2 ==0):
+        arrE.append(i)
+    else:
+        arrO.append(i)
+print(arrE)
+print(arrO)
+    
