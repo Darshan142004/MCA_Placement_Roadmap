3aa7d23e4971c28e6ccf42e2fb5a23529e9db25c
Darshan142004 <darshanbhagwat14@gmail.com>
2026-02-09 23:27:40 +0530

    update

--- MCA_DSA/Array/MAxDiff.py
+++ MCA_DSA/Array/MAxDiff.py
@@ -2,7 +2,6 @@ arr = [2, 3, 10, 6, 4, 8, 1]
 
 min_so_far = arr[0]
 max_diff = arr[1] - arr[0]
-
 for i in range(1, len(arr)):
     max_diff = max(max_diff, arr[i] - min_so_far)
     min_so_far = min(min_so_far, arr[i])
