bc997f599b19e9571d690a409b15209116b4c879
Darshan142004 <darshanbhagwat14@gmail.com>
2026-02-09 23:25:09 +0530

    update

--- MCA_DSA/Array/bubblesort.py
+++ MCA_DSA/Array/bubblesort.py
@@ -4,6 +4,4 @@ for i in range(n-1):         # Total passes
     for j in range(n-1-i):   # Compare adjacent
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]  # Swap
-
-
 print("Bubble Sorted:", arr)
