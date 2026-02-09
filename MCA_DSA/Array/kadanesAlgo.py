e842e51a3c7f6a2f4025c60851808b60d8b126d2
Darshan142004 <darshanbhagwat14@gmail.com>
2026-02-09 23:27:06 +0530

    update

--- MCA_DSA/Array/kadanesAlgo.py
+++ MCA_DSA/Array/kadanesAlgo.py
@@ -3,6 +3,8 @@ arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 max_sum = arr[0]
 current = arr[0]
 
+
+
 for i in range(1, len(arr)):
     current = max(arr[i], current + arr[i])
     max_sum = max(max_sum, current)
